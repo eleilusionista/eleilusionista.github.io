7ed3d266983ea55a1183307377a8b8c035234adf
eleilusionista <125239928+eleilusionista@users.noreply.github.com>
2023-11-09 15:24:08 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function redirigirADonacion() {
+  // Aquí debes redirigir a la página de donación o mostrar los detalles bancarios.
+  // Puedes usar window.location.href o alguna otra forma de redirección.
+  // Asegúrate de implementar esto de manera segura.
+}
